9d5782996080fda124f8817548c7531c9df4f26b
enxie <enxie@students.cs.ubc.ca>
2021-06-13 16:29:10 -0700

    added api to retrieve bet info

---
+++ objects/bet.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+class Bet {
+    private $database;
+    private $table_name_1 = "\"Place_bets_on\"";
+    private $table_name_2 = "\"Horse_ridden_by\"";
+
+    public function __construct($db) {
+        $this->database = $db;
+    }
+
+    public $accountID;
+
+    function getBetInfo() {
+        $query = "SELECT BETID, p.HORSEID, NICKNAME, ACCOUNTID, AMOUNT, BET_DATE, BET_TYPE FROM " . 
+        $this->table_name_1 . " p, " . $this->table_name_2 . " h" . 
+        " WHERE p.ACCOUNTID = " . $this->accountID . " AND " . 
+        "p.HORSEID=h.HORSEID";
+        $stmt = $this->database->executePlainSQL($query);
+        return $stmt;
+    }
+}
+?>
---
+++ transaction/customerBetInfo.php
@@ -0,0 +1,57 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once "../config/database.php";
+include_once "../objects/bet.php";
+
+$database = new Database();
+$db = $database->connectToDB();
+
+// Instantiate new customer object
+$bet = new Bet($database);
+
+// Instantiate customer's accountID
+$bet->accountID = $_GET['accountid'];
+
+// Attempt fetching all customers
+$stmt = $bet->getBetInfo();
+$nrows = oci_fetch_all($stmt, $res);
+
+// Instantiate records array and push customer objects using loop
+if ($nrows > 0) {
+    $customers_bet_info = array();
+    $customers_bet_info["records"] = array();
+
+    for ($i = 0; $i < $nrows; $i++) {
+        $customer_i = array(
+            "betID" => $res["BETID"][$i],
+            "horseID" => $res["HORSEID"][$i],
+            "accountID" => $res["ACCOUNTID"][$i],
+            "amount" => $res["AMOUNT"][$i],
+            "bet_date" => $res["BET_DATE"][$i],
+            "bet_type" => $res["BET_TYPE"][$i]
+        );
+
+        array_push($customers_bet_info["records"], $customer_i);
+    }
+
+    oci_free_statement($stmt);
+
+    http_response_code(200);
+
+    echo json_encode($customers_bet_info);
+} else {
+    http_response_code(404);
+
+    echo json_encode(
+        array("message" => "No customers found.")
+    );
+}
+
+$database->disconnectFromDB();
+?>
