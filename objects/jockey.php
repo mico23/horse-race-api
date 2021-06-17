e7741f55c881c99c7a287f1524974ee61d1fc4a3
mico23 <en.xie.michael@gmail.com>
2021-06-16 20:47:27 -0700

    updated jockey api

--- objects/jockey.php
+++ objects/jockey.php
@@ -2,9 +2,11 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-class Customer {
+class Jockey {
     private $database;
-    private $table_name = "Jockey";
+    private $table_name_1 = "JOCKEY_MEMBEROF";
+    private $table_name_2 = "HORSECLUB";
+    private $table_name_3 = "HORSE_RIDDEN_BY";
     
     public $jockeyID;
     public $name;
@@ -15,23 +17,28 @@ class Customer {
         $this->database = $db;
     }
 
+    //select j.jockeyid, j.YEARS_OF_EXP, j.NAME, HORSE_CLUB_ID, count(*) num_of_horse_ridden from HORSE_RIDDEN_BY h, JOCKEY_MEMBEROF j where h.jockeyid=j.jockeyid GROUP BY j.jockeyid, j.YEARS_OF_EXP, j.NAME, HORSE_CLUB_ID ORDER BY J.JOCKEYID;
     function read() {
-        $query = "SELECT * FROM " . $this->table_name;
+        $query = "SELECT j.jockeyid, j.YEARS_OF_EXP, j.NAME, HORSE_CLUB_ID, count(*) num_of_horse_ridden FROM " . 
+        $this->table_name_1 . " j INNER JOIN " . $this->table_name_3 . " h" . 
+        " ON h.jockeyid=j.jockeyid GROUP BY j.jockeyid, j.YEARS_OF_EXP, j.NAME, HORSE_CLUB_ID ORDER BY J.JOCKEYID";
         $stmt = $this->database->executePlainSQL($query);
 
         return $stmt;
     }
 
-    // modify this to join membership table
+    //select j.jockeyid, j.years_of_exp, j.name jockeyName, h.name clubName from JOCKEY_MEMBEROF j, HORSECLUB h where j.horse_club_id=h.horse_club_id;
     function getJockeyInfo() {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE jockeyID = " . $this->jockeyID;
+        $query = "SELECT j.jockeyid, j.years_of_exp, j.name jockeyName, h.name clubName FROM " . $this->table_name_1 . " j INNER JOIN " . $this->table_name_2 . " h"
+        . " ON jockeyID = " . $this->jockeyID . " AND j.horse_club_id=h.horse_club_id";
+
         $stmt = $this->database->executePlainSQL($query);
 
         return $stmt;
     }
 
     function addJockey() {
-        $query = "INSERT INTO " . $this->table_name . 
+        $query = "INSERT INTO " . $this->table_name_1 . 
             " (jockeyID, name, yoe, club) VALUES ('" . 
             $this->jockeyID . "', '" . $this->name . "', '" . $this->yoe . "', '" . $this->club . "')";
         $stmt = $this->database->executePlainSQL($query);
@@ -40,7 +47,7 @@ class Customer {
     }
 
     function createJockey() {
-        $query = "INSERT INTO " . $this->table_name . 
+        $query = "INSERT INTO " . $this->table_name_1 . 
             " (jockeyID, name, yoe, club) VALUES ('" . 
             $this->jockeyID . "', '" . $this->name . "', '" . $this->yoe . "', '" . $this->club . "')";
         $stmt = $this->database->executePlainSQL($query);
